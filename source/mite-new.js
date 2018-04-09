18753b4d44ed4608eb46f1465fc1d99dcef6db14
Ephigenia <love@ephigenia.de>
2018-04-09 10:22:45 +0200

    refactor(new): promisified some of the code

--- source/mite-new.js
+++ source/mite-new.js
@@ -6,6 +6,7 @@ const inquirer = require('inquirer')
 const parallel = require('async/parallel')
 const chalk = require('chalk')
 const miteApi = require('mite-api')
+const bluebird = require('bluebird');
 
 const pkg = require('./../package.json')
 const config = require('./config.js')
@@ -25,54 +26,53 @@ if (process.argv.length <= 2) {
   main(null);
 }
 
+function getProjectChoices() {
+  return bluebird.promisify(mite.getProjects)()
+    .then(response => response.map(d => d.project))
+    .then(projects => projects.map(project => {
+      const nameParts = [project.name];
+      if (project.customer_name) {
+        nameParts.push('(' + project.customer_name + ')')
+      }
+      return {
+        name: nameParts.join(' '),
+        value: project.id
+      };
+    }))
+    .then(projects => {
+      projects.push({
+        name: '– (no project)',
+        value: null,
+      })
+      return projects;
+    });
+}
+
+function getServiceChoices() {
+  return bluebird.promisify(mite.getServices)()
+    .then(response => response.map(d => d.service))
+    .then(services => services.map(service => {
+      const nameParts = [service.name];
+      if (service.billable) {
+        nameParts.push(chalk.yellow.bold('$'));
+      }
+      return { name: nameParts.join(' '), value: service.id};
+    }))
+    .then(services => {
+      services.push({
+        name: '– (no service)',
+        value: null,
+      })
+      return services;
+    });
+}
+
 function main(note) {
-
-  parallel({
-    projects: mite.getProjects,
-    services: mite.getServices
-  }, (err, results) => {
-    if (err) {
-      throw err;
-    }
-
-    // compile a list of possible project options including an empty option
-    // which doesn’t set the project for the newly created time entry
-    const projectChoices = results.projects
-      .map(function(data) {
-        var nameParts = [data.project.name];
-        if (data.project.customer_name) {
-          nameParts.push('(' + data.project.customer_name + ')')
-        }
-        return {
-          name: nameParts.join(' '),
-          value: data.project.id
-        };
-      })
-      .sort();
-    projectChoices.push({
-      name: '– (no project)',
-      value: null,
-    });
-
-    // compile a list of
-    const servicesChoices = results.services
-      .map(function(data) {
-        let name = data.service.name;
-        if (data.service.billable) {
-          name += chalk.yellow.bold('$')
-        }
-        return {
-          name: name,
-          value: data.service.id
-        };
-      })
-      .sort();
-    servicesChoices.push({
-      name: '– (no service)',
-      value: null,
-    });
-
-    const questions = [
+  return Promise.all([
+    getProjectChoices(),
+    getServiceChoices(),
+  ]).then(([projectChoices, serviceChoices]) => {
+    return [
       {
         type: 'list',
         name: 'project_id',
@@ -94,7 +94,7 @@ function main(note) {
         type: 'list',
         name: 'service_id',
         message: 'What service?',
-        choices: servicesChoices,
+        choices: serviceChoices,
       },
       {
         type: 'input',
@@ -102,44 +102,44 @@ function main(note) {
         message: 'Date?',
         default: (new Date()).toISOString().slice(0,10)
       }
-    ];
+    ]
+  })
+  .then((questions) => inquirer.prompt(questions))
+  .then((entry) => {
+    // do not create an entry when minutes are invalid
+    if (!entry.minutes) {
+      console.log('no time entry created due to empty project id or empty minutes')
+      return;
+    }
 
-    inquirer.prompt(questions).then((entry) => {
-      // do not create an entry when minutes are invalid
-      if (!entry.minutes) {
-        console.log('no time entry created due to empty project id or empty minutes')
-        return;
+    // detect the "+" at the duration value and start a tracker for the time
+    // entry
+    let startTracker = false
+    if (entry.minutes.substr(-1, 1) === '+') {
+      startTracker = true
+      entry.minutes = entry.minutes.substr(0, entry.minutes.length - 1)
+    }
+
+    // http://mite.yo.lk/en/api/time-entries.html#create
+    mite.addTimeEntry({ time_entry: entry }, (response) => {
+      var data = JSON.parse(response);
+      if (data.error) {
+        console.error('Error while creating new time entry:', data.error)
+        process.exit(1)
+        return
       }
-
-      // detect the "+" at the duration value and start a tracker for the time
-      // entry
-      let startTracker = false
-      if (entry.minutes.substr(-1, 1) === '+') {
-        startTracker = true
-        entry.minutes = entry.minutes.substr(0, entry.minutes.length - 1)
+      const timeEntryId = data.time_entry.id;
+      console.log('Successfully created new time entry (id: %s)', timeEntryId);
+      if (startTracker) {
+        miteTracker.start(timeEntryId)
+          .then(() => {
+            console.log('Successfully started the time entry (id: %s)', timeEntryId);
+          })
+          .catch((err) => {
+            console.log('Unable to start the time entry (id: %s): %s', timeEntryId, err.message);
+            process.exit(1);
+          })
       }
-
-      // http://mite.yo.lk/en/api/time-entries.html#create
-      mite.addTimeEntry({ time_entry: entry }, (response) => {
-        var data = JSON.parse(response);
-        if (data.error) {
-          console.error('Error while creating new time entry:', data.error)
-          process.exit(1)
-          return
-        }
-        const timeEntryId = data.time_entry.id;
-        console.log('Successfully created new time entry (id: %s)', timeEntryId);
-        if (startTracker) {
-          miteTracker.start(timeEntryId)
-            .then(() => {
-              console.log('Successfully started the time entry (id: %s)', timeEntryId);
-            })
-            .catch((err) => {
-              console.log('Unable to start the time entry (id: %s): %s', timeEntryId, err.message);
-              process.exit(1);
-            })
-        }
-      })
-    }) // inquirer
-  }) // parallel
+    });
+  }) // inquirer
 }
