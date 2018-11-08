85d4e817adc50526d641192a00e9ee8a14d446a4
Ephigenia <love@ephigenia.de>
2018-11-08 14:42:30 +0100

    feat(projects): adds format option

--- source/mite-projects.js
+++ source/mite-projects.js
@@ -5,9 +5,8 @@ const program = require('commander');
 const miteApi = require('mite-api');
 const chalk = require('chalk');
 const async = require('async');
-const tableLib = require('table')
-const table = tableLib.table;
 
+const DataOutput = require('./lib/data-output');
 const pkg = require('./../package.json');
 const config = require('./config.js');
 const formater = require('./lib/formater');
@@ -70,6 +69,11 @@ program
       return ['true', 'yes', 'ja', 'ok', '1'].indexOf(val.toLowerCase()) > -1;
     }),
     false // default archive argument
+  )
+  .option(
+    '-f, --format <format>',
+    'defines the output format, valid options are ' + DataOutput.FORMATS.join(', '),
+    'table',
   )
   .parse(process.argv);
 
@@ -162,24 +166,21 @@ async.parallel([
     'rate',
     'note',
   ].map((v) => chalk.bold(v)));
-  const tableConfig = {
-    border: tableLib.getBorderCharacters('norc'),
-    columns: {
-      1: {
-        wrapWord: true,
-      },
-      3: {
-        alignment: 'right',
-      },
-      4: {
-        alignment: 'right',
-      },
-      5: {
-        width: 50,
-        wrapWord: true,
-      }
+  const columns = {
+    1: {
+      wrapWord: true,
+    },
+    3: {
+      alignment: 'right',
+    },
+    4: {
+      alignment: 'right',
+    },
+    5: {
+      width: 50,
+      wrapWord: true,
     }
   };
 
-  console.log(table(tableData, tableConfig));
+  console.log(DataOutput.formatData(tableData, program.format, columns));
 });
