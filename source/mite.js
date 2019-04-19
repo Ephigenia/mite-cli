93ad79e86c9d33ae4d3feb45b3100de32f19d237
Ephigenia <love@ephigenia.de>
2019-04-19 12:49:29 +0200

    fix(mite): accidential dublicate parsing of argv

--- source/mite.js
+++ source/mite.js
@@ -8,25 +8,22 @@ const autoComplete = require('./lib/auto-complete');
 
 program
   .version(pkg.version)
-  .description(pkg.description)
   .command('amend', 'edit the text for a specific time entry or the currently runnning entry').alias('reword')
   .command('autocomplete', 'install/uninstall autocompletion')
   .command('config', 'show or set configuration settings')
   .command('customers', 'list, filter & search customers').alias('clients')
-  .command('customers', 'list, filter & search customers').alias('clients')
   .command('delete', 'delete a specific time entry').alias('rm')
   .command('list', 'list time entries', { isDefault: true }).alias('ls').alias('status').alias('st')
-  .command('lock', 'lock a single time entry')
-  .command('new', 'create a new time entry').alias('create')
+  .command('lock', 'lock single time entry')
   .command('new', 'create a new time entry').alias('create')
   .command('open', 'open the given time entry in browser')
   .command('projects', 'list, filter & search projects')
   .command('services', 'list, filter & search services')
   .command('start', 'start the tracker for the given id, will also stop allready running entry')
   .command('stop', 'stop any running counter')
-  .command('unlock', 'unlock a single time entry')
+  .command('unlock', 'unlock single time entry')
   .command('users', 'list, filter & search for users')
-  .parse(process.argv);
+  .description(pkg.description);
 
 // completion command is triggered by automatically installed autocompletion
 // lib and the appropriate shell, it’s not listed in the commands above as
