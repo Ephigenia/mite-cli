c410ab263bfcac30edaeeea148b6e27fecb7af66
Ephigenia <love@ephigenia.de>
2019-04-19 12:46:55 +0200

    feat(lock): adds auto-completion for options

--- source/lib/auto-complete/completions/index.js
+++ source/lib/auto-complete/completions/index.js
@@ -10,5 +10,6 @@ module.exports = {
   new: require('./new'),
   projects: require('./projects'),
   services: require('./services'),
+  unlock: require('./unlock'),
   users: require('./users'),
 };
